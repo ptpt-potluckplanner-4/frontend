77b95884e549ac357d53d5bd9a7e3e2598868bdf
markporo <markporo@yahoo.com>
2021-08-20 00:28:01 -0400

    planning and organizing front end

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+//Mark does this
+// logout - profile - CREATE POTLUCK.JS - ATTEND POTLUCK - 
+
+export default function NavBar() {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,2 +1,29 @@
-//shows potlucks the users is going to attend
-//shows potluck the user is organizing
+//Chase builds this
+
+//shows potlucks the users is going to attend ---- attendPotluckList
+//shows potluck the user is organizing --- Organizing
+
+
+//Organizing button/ Attending button
+
+// whichever user clicks on...shows that information
+
+// organizing button clicked --- shows all users organized potlucks 
+// if no event is shown then a button shows that links to createpotluck.js
+// if currentUser === organizer than allow edit capabilities
+
+// if attending is clicked --- hides users events/ shows all available potlucks
+// if user has not signed up to attend a potluck then button shows 
+// and links to potLuckList.js (edit capabilities disabled)
+
+import NavBar from "../NavBar/NavBar"
+
+export default function Profile() {
+
+    return (
+        <div>
+            <NavBar />
+        </div>
+    )
+}
+
