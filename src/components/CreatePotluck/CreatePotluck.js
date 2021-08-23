dda6dd4dd735b5844ef26b3a357ca3623ab3da30
markporo <markporo@yahoo.com>
2021-08-22 22:09:33 -0400

    working on axios createpotluck

--- src/components/CreatePotluck/CreatePotluck.js
+++ src/components/CreatePotluck/CreatePotluck.js
@@ -39,21 +39,17 @@ export default function CreatePotluckForm() {
     // allows you to use styles from Material UI
     const classes = useStyles();
 
-    // initial url
-    const url = "https://potluck-planner-04.herokuapp.com/potlucks/create"
-
     // initial potluck state
     const initialPotluckState = {
         title: "",
         date: "",
         time: "",
         location: "",
-        organizer: "",
         food: [],
     }
 
     //initial food state
-    // const initialFoodState = []; // { id: "", foodName: "" }
+    const initialFoodState = []; // { id: "", foodName: "" }
 
     // initial create potluck errors
     const initialCreatePotluckErrors = {
@@ -65,9 +61,9 @@ export default function CreatePotluckForm() {
         food: "",
     }
 
-    // state for potluckData
+    // state for potluckData ---potluckData === form values
     const [potluckData, setPotluckData] = useState(initialPotluckState)
-    const foodItemArray = [];
+    const [foodItemArray, setFoodItemArray] = useState(initialFoodState);
     const [createPotluckErrors, setCreatePotluckErrors] = useState(initialCreatePotluckErrors)
 
 
@@ -76,18 +72,38 @@ export default function CreatePotluckForm() {
         e.preventDefault();
 
         // send to database via axios
-        // Axios.post(url, {
-        //     title: potluckData.title,
-        //     date: potluckData.date,
-        //     time: potluckData.time,
-        //     location: potluckData.location,
-        //     organizer: potluckData.organizer,
-        //     foods: potluckData.foods
-        // })
-        //     .then(res => {
-        //         console.log(res.data)
-        //     })
+        Axios.post('https://potluck-planner-04.herokuapp.com/potlucks/', {
+            title: potluckData.title,
+            date: potluckData.date,
+            time: potluckData.time,
+            location: potluckData.location,
+            organizer: 1,
+            // food: foodItemArray,
+        })
+            .then(res => {
+                console.log(res.data)
+                setPotluckData(res.data);
+            })
+            .finally(res => {
+                setPotluckData(initialPotluckState);
+            }
+            )
     }
+
+
+    // food Items sent to Database
+    // Axios.post('https://potluck-planner-04.herokuapp.com/potlucks/1/foods', {
+    //     food_name: ,
+    //     contributor: 1,
+    // })
+    //     .then(res => {
+    //         console.log(res.data)
+    //         setPotluckData(res.data);
+    //     })
+    //     .finally(res => {
+    //         setPotluckData(initialPotluckState);
+    //     }
+    //     )
 
 
     return (<div>
@@ -112,10 +128,7 @@ export default function CreatePotluckForm() {
                     <label htmlFor='location'>Location</label>
                     <input type='text' name='location' id='location' onChange={e => setPotluckData({ ...potluckData, location: e.target.value })} value={potluckData.location} />
                 </div>
-                <div className='form-group'>
-                    <label htmlFor='organizer'>Organizer</label>
-                    <input type='text' name='organizer' id='date' onChange={e => setPotluckData({ ...potluckData, organizer: e.target.value })} value={potluckData.organizer} />
-                </div>
+
 
 
                 <button
@@ -139,6 +152,7 @@ export default function CreatePotluckForm() {
                         {foodItemArray.map(eachFoodItem => {
                             return (
                                 <li>{eachFoodItem} <button>Delete</button></li>
+
                             )
                         })}
                     </ul>
